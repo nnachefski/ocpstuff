9a62a9f87c47dd0f3eafd8f2e871cb97a4d1cb0f
nick <nick@hv4.home.nicknach.net>
2017-12-25 10:22:26 -0600

    test

--- images/import-images.py
+++ images/import-images.py
@@ -81,7 +81,7 @@ for i in list:
 		sys.exit(1)
 	except:
 	 	print("failed to inspect %s/%s:%s"%(src_registry, i, tag))
-	 	print(STDOUT)
+	 	print(DEVNULL)
 	 	continue
 	else:
 		#print("inspected %s/%s:%s"%(src_registry, i, tag))
