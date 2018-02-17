5eb378acf23c0863cc15925ad7bd7b3564ed9e2d
nick <nick@metal.home.nicknach.net>
2018-02-17 10:56:09 -0600

    added instructions comment

--- sync_repos.sh
+++ sync_repos.sh
@@ -1,6 +1,7 @@
+## this scripts is used to syncronize my local repo server with the upstream RHN repos.  Cron this to run nightly
+
 export REPODIR=/var/www/html/repo
 
-# rhel-server-rhscl-7-rpms
 for repo in nvidia rhel-7-fast-datapath-rpms rhel-7-server-ose-3.7-rpms rhel-server-rhscl-7-rpms rhel-7-server-optional-rpms rh-gluster-3-for-rhel-7-server-rpms rhel-7-server-extras-rpms rhel-7-server-rpms epel;
 do
   echo "############## ${repo} ##################"
