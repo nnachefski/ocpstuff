7863acbfd103bcee85d5a7d4e0b48338c41f4fe7
nick <nick@metal.home.nicknach.net>
2018-03-10 11:25:07 -0600

    block on lb deploy

--- lab_scripts/deploy_lab.sh
+++ lab_scripts/deploy_lab.sh
@@ -20,13 +20,13 @@ if [ "$TYPE" == "oso" ]; then
 
 elif [ "$TYPE" == "ocp" ]; then
 	echo "- using $TEMPLATE base image"
+        ssh root@storage.home.nicknach.net /cloud/scripts/new.sh lb $TEMPLATE 4196 2 52:54:00:18:58:16
 	ssh root@hv4.home.nicknach.net /cloud/scripts/new.sh master01 $TEMPLATE 16384 2 52:54:00:fb:09:ec &
 	ssh root@hv3.home.nicknach.net /cloud/scripts/new.sh master02 $TEMPLATE 16384 2 52:54:00:18:58:01 &
 	ssh root@hv5.home.nicknach.net /cloud/scripts/new.sh master03 $TEMPLATE 16384 2 52:54:00:18:58:02 &
 	ssh root@hv4.home.nicknach.net /cloud/scripts/new.sh node01 $TEMPLATE 16384 2 52:54:00:db:14:7d &
 	ssh root@hv3.home.nicknach.net /cloud/scripts/new.sh node02 $TEMPLATE 16384 2 52:54:00:68:4a:e3 &
 	ssh root@hv5.home.nicknach.net /cloud/scripts/new.sh node03 $TEMPLATE 16384 2 52:54:00:68:54:49 &
-	ssh root@storage.home.nicknach.net /cloud/scripts/new.sh lb $TEMPLATE 4196 2 52:54:00:18:58:16 &
 	ssh root@hv4.home.nicknach.net /cloud/scripts/new.sh infra01 $TEMPLATE 16384 2 52:54:00:18:58:03 &
 	ssh root@hv3.home.nicknach.net /cloud/scripts/new.sh infra02 $TEMPLATE 16384 2 52:54:00:18:59:04 &
 	ssh root@hv5.home.nicknach.net /cloud/scripts/new.sh infra03 $TEMPLATE 16384 2 52:54:00:18:73:04 &
@@ -36,5 +36,4 @@ else
 fi
 
 echo "Done!"
-
 
