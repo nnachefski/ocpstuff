4b4bfa7ee692daa3e97ab71afe501a5b61878fab
nick <nick@metal.home.nicknach.net>
2018-03-15 10:42:00 -0500

    removed puddle stuff

--- scripts/sync_repos.sh
+++ scripts/sync_repos.sh
@@ -2,7 +2,7 @@
 
 export REPODIR=repo
 
-for repo in rhaos-3.9 nvidia rhel-7-fast-datapath-rpms rhel-7-server-ose-3.7-rpms rhel-server-rhscl-7-rpms rhel-7-server-optional-rpms rh-gluster-3-for-rhel-7-server-rpms rhel-7-server-extras-rpms rhel-7-server-rpms epel;
+for repo in nvidia rhel-7-fast-datapath-rpms rhel-7-server-ose-3.7-rpms rhel-server-rhscl-7-rpms rhel-7-server-optional-rpms rh-gluster-3-for-rhel-7-server-rpms rhel-7-server-extras-rpms rhel-7-server-rpms epel;
 do
   echo "############## ${repo} ##################"
   reposync --gpgcheck -lm --repoid=${repo} --download_path=$REPODIR
