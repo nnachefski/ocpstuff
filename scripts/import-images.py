07226af8efbb0cb3b0e0c357e116386fce540e70
nick <nick@metal.home.nicknach.net>
2018-06-30 23:45:21 -0500

    removed debug line

--- scripts/import-images.py
+++ scripts/import-images.py
@@ -75,7 +75,6 @@ for image in list:
 
 # iterate over the pass list and copy the images
 for image in pass_list:
-	print(image)
 	cmdline = ['skopeo', '--insecure-policy', 'copy', '--dest-cert-dir=/etc/docker/certs.d/repo.home.nicknach.net', '--src-tls-verify=false', '--dest-tls-verify=false', "docker://%s/%s"%(args.source, image), "%s/%s"%(uri_string, image)]
 	if args.d: print('- '+' '.join(cmdline))
 	try:
