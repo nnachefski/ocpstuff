70eb13a6ced4f1aef899c8bd0c0d3cceb01eded8
nick <nick@desktop.home.nicknach.net>
2019-01-11 15:47:29 -0600

    updated firstboot

--- satellite/firstboot/firstboot.sh
+++ satellite/firstboot/firstboot.sh
@@ -1,11 +1,12 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
 cd /root
 echo "starting OCP install" && sleep 60
-git clone https://github.com/openshift/openshift-ansible.git --branch release-3.11
-ansible-playbook openshift-ansible/playbooks/prerequisites.yml || exit 1
-ansible-playbook openshift-ansible/playbooks/deploy_cluster.yml || exit 1
-#ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml 
-#ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml 
+sed -i 's/#log_path/log_path/' /etc/ansible/ansible.cfg
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml  || exit 1
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml || exit 1
+#git clone https://github.com/openshift/openshift-ansible.git --branch release-3.11
+#ansible-playbook openshift-ansible/playbooks/prerequisites.yml || exit 1
+#ansible-playbook openshift-ansible/playbooks/deploy_cluster.yml || exit 1
 wget https://raw.githubusercontent.com/nnachefski/ocpstuff/master/rbac/ocp_group_sync.conf -O /etc/origin/master/ocp_group_sync.conf
 wget https://raw.githubusercontent.com/nnachefski/ocpstuff/master/rbac/ocp_group_sync-whitelist.conf -O /etc/origin/master/ocp_group_sync-whitelist.conf 
 oc adm groups sync --sync-config=/etc/origin/master/ocp_group_sync.conf --confirm --whitelist=/etc/origin/master/ocp_group_sync-whitelist.conf
@@ -18,10 +19,11 @@ oc get cm node-config-all-in-one -n openshift-node -o yaml |sed '/RotateKubeletC
 oc replace cm node-config-all-in-one -f node-config.patch -n openshift-node
 
 cat <<EOF > post.sh
-cp /etc/origin/node/pods/apiserver.yaml apiserver.yaml.prepatch
-oc ex config patch apiserver.yaml.prepatch -p '{"spec":{"containers":[{"name":"api","volumeMounts":[{"mountPath":"/etc/pki","name":"certs"}]}],"volumes":[{"hostPath":{"path":"/etc/pki","type":"Directory"},"name":"certs"}]}}' > /etc/origin/node/pods/apiserver.yaml 
+cd /root
+cp /etc/origin/node/pods/apiserver.yaml /root/apiserver.yaml.prepatch
+oc ex config patch /root/apiserver.yaml.prepatch -p '{"spec":{"containers":[{"name":"api","volumeMounts":[{"mountPath":"/etc/pki","name":"certs"}]}],"volumes":[{"hostPath":{"path":"/etc/pki","type":"Directory"},"name":"certs"}]}}' > /etc/origin/node/pods/apiserver.yaml 
 wget https://raw.githubusercontent.com/Maistra/openshift-ansible/maistra-0.6/istio/master-config.patch
-cp -p /etc/origin/master/master-config.yaml master-config.yaml.prepatch
+cp -f /etc/origin/master/master-config.yaml master-config.yaml.prepatch
 oc ex config patch master-config.yaml.prepatch -p "$(cat master-config.patch)" > /etc/origin/master/master-config.yaml
 /usr/local/bin/master-restart api && /usr/local/bin/master-restart controllers
 echo 'vm.max_map_count = 262144' > /etc/sysctl.d/99-elasticsearch.conf
