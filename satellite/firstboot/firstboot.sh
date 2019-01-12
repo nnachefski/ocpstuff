b87828c4c808ca22d2bc987d9b8879ae2b03ac7e
nick <nick@desktop.home.nicknach.net>
2019-01-12 00:25:40 -0600

    fixed firstboot.sh

--- satellite/firstboot/firstboot.sh
+++ satellite/firstboot/firstboot.sh
@@ -1,12 +1,11 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
-cd /root
 echo "starting OCP install" && sleep 60
 sed -i 's/#log_path/log_path/' /etc/ansible/ansible.cfg
-ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml  || exit 1
-ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml || exit 1
 #git clone https://github.com/openshift/openshift-ansible.git --branch release-3.11
 #ansible-playbook openshift-ansible/playbooks/prerequisites.yml || exit 1
 #ansible-playbook openshift-ansible/playbooks/deploy_cluster.yml || exit 1
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml  || exit 1
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml || exit 1
 wget https://raw.githubusercontent.com/nnachefski/ocpstuff/master/rbac/ocp_group_sync.conf -O /etc/origin/master/ocp_group_sync.conf
 wget https://raw.githubusercontent.com/nnachefski/ocpstuff/master/rbac/ocp_group_sync-whitelist.conf -O /etc/origin/master/ocp_group_sync-whitelist.conf 
 oc adm groups sync --sync-config=/etc/origin/master/ocp_group_sync.conf --confirm --whitelist=/etc/origin/master/ocp_group_sync-whitelist.conf
@@ -18,20 +17,6 @@ oc adm policy add-scc-to-user hostaccess -z registry -n default
 oc get cm node-config-all-in-one -n openshift-node -o yaml |sed '/RotateKubeletClientCertificate/ s/$/,DevicePlugins=true/' > node-config.patch
 oc replace cm node-config-all-in-one -f node-config.patch -n openshift-node
 
-cat <<EOF > post.sh
-cd /root
-cp /etc/origin/node/pods/apiserver.yaml /root/apiserver.yaml.prepatch
-oc ex config patch /root/apiserver.yaml.prepatch -p '{"spec":{"containers":[{"name":"api","volumeMounts":[{"mountPath":"/etc/pki","name":"certs"}]}],"volumes":[{"hostPath":{"path":"/etc/pki","type":"Directory"},"name":"certs"}]}}' > /etc/origin/node/pods/apiserver.yaml 
-wget https://raw.githubusercontent.com/Maistra/openshift-ansible/maistra-0.6/istio/master-config.patch
-cp -f /etc/origin/master/master-config.yaml master-config.yaml.prepatch
-oc ex config patch master-config.yaml.prepatch -p "$(cat master-config.patch)" > /etc/origin/master/master-config.yaml
-/usr/local/bin/master-restart api && /usr/local/bin/master-restart controllers
-echo 'vm.max_map_count = 262144' > /etc/sysctl.d/99-elasticsearch.conf
-chcon -R -t container_file_t  /dev/nvidia*
-restorecon -R /dev/nvidia*
-setenforce 0 && sed -i 's/=enforcing/=permissive/' /etc/selinux/config
-EOF
-
-chmod +x post.sh
+curl https://raw.githubusercontent.com/nnachefski/ocpstuff/master/satellite/firstboot/post-install.sh > post-install.sh && chmod +x post-install.sh
 ansible "*" -m script -a "post.sh"
 
