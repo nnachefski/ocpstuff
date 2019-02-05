b6d6f0da10324ad6f8a5b0519b28718c71633556
nick <nick@desktop.home.nicknach.net>
2019-02-05 13:43:39 -0600

    disabled api patch

--- satellite/firstboot/post-install.sh
+++ satellite/firstboot/post-install.sh
@@ -1,10 +1,10 @@
 cd /root
-cp /etc/origin/node/pods/apiserver.yaml apiserver.yaml.prepatch
-oc ex config patch apiserver.yaml.prepatch -p '{"spec":{"containers":[{"name":"api","volumeMounts":[{"mountPath":"/etc/pki","name":"certs"}]}],"volumes":[{"hostPath":{"path":"/etc/pki","type":"Directory"},"name":"certs"}]}}' > /etc/origin/node/pods/apiserver.yaml      
+#cp /etc/origin/node/pods/apiserver.yaml apiserver.yaml.prepatch
+#oc ex config patch apiserver.yaml.prepatch -p '{"spec":{"containers":[{"name":"api","volumeMounts":[{"mountPath":"/etc/pki","name":"certs"}]}],"volumes":[{"hostPath":{"path":"/etc/pki","type":"Directory"},"name":"certs"}]}}' > /etc/origin/node/pods/apiserver.yaml      
 #curl https://raw.githubusercontent.com/Maistra/openshift-ansible/maistra-0.6/istio/master-config.patch > master-config.patch
 #cp -f /etc/origin/master/master-config.yaml master-config.yaml.prepatch
 #oc ex config patch master-config.yaml.prepatch -p "$(cat master-config.patch)" > /etc/origin/master/master-config.yaml
-/usr/local/bin/master-restart api && /usr/local/bin/master-restart controllers
+#/usr/local/bin/master-restart api && /usr/local/bin/master-restart controllers
 #echo 'vm.max_map_count = 262144' > /etc/sysctl.d/99-elasticsearch.conf
 #chcon -R -t container_file_t  /dev/nvidia*
 #restorecon -R /dev/nvidia*
