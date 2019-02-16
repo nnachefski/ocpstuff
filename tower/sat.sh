87d50ef2342dcd0c75edc5b292d0f009530ca499
nick <nick@desktop.home.nicknach.net>
2019-02-16 16:00:32 -0600

    test

---
+++ tower/sat.sh
@@ -0,0 +1,1 @@
+curl http://satellite.home.nicknach.net/pub/bootstrap.py > bootstrap.py && chmod +x bootstrap.py && ./bootstrap.py -l admin -o nicknach -a ocp-node -s satellite.home.nicknach.net -L home -g ocp-nodes -p welcome1 --skip-puppet --force
