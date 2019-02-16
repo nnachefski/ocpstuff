f6766ce6d3de49d3588c7e9ce8ac73f08f16dda6
nick <nick@desktop.home.nicknach.net>
2019-02-16 16:02:17 -0600

    add epel disable

--- tower/sat.sh
+++ tower/sat.sh
@@ -1,1 +1,5 @@
 curl http://satellite.home.nicknach.net/pub/bootstrap.py > bootstrap.py && chmod +x bootstrap.py && ./bootstrap.py -l admin -o nicknach -a ocp-node -s satellite.home.nicknach.net -L home -g ocp-nodes -p welcome1 --skip-puppet --force
+
+subscription-manager repos --disable nicknach_epel_epel
+
+subscription-manager repos --disable nicknach_nvidia_cuda
