be21bd121e06cc7fc6990de2d3c03531cc5a95d9
nick <nick@desktop.home.nicknach.net>
2019-02-16 12:43:21 -0600

    added ldap.py for tower

---
+++ tower/ldap.py
@@ -0,0 +1,26 @@
+{
+  "AUTH_LDAP_SERVER_URI": "ldap://gw.home.nicknach.net",
+  "AUTH_LDAP_BIND_DN": "",
+  "AUTH_LDAP_BIND_PASSWORD": "",
+  "AUTH_LDAP_CONNECTION_OPTIONS": {
+    "OPT_NETWORK_TIMEOUT": 30,
+    "OPT_REFERRALS": 0
+  },
+  "AUTH_LDAP_USER_SEARCH": [
+    "cn=users,cn=accounts,dc=home,dc=nicknach,dc=net",
+    "SCOPE_SUBTREE",
+    "(uid=%(user)s)"
+  ],
+  "AUTH_LDAP_USER_ATTR_MAP": {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail"
+  },
+  "AUTH_LDAP_GROUP_SEARCH": [
+    "cn=tower_users,cn=groups,cn=accounts,dc=home,dc=nicknach,dc=net",
+    "SCOPE_SUBTREE",
+    "(objectClass=group)"
+  ],
+  "AUTH_LDAP_USER_FLAGS_BY_GROUP": {
+  }
+}
